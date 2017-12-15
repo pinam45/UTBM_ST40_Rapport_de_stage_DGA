d89efad59a7daaba42bb2d9e3ada3f11b86cf300
Maxime Pinard <maxime.pin@live.fr>
2017-12-15 22:44:12 +0100

    Ajout de make.cmd et clean.cmd pour la compilation sous Windows

---
+++ clean.cmd
@@ -0,0 +1,10 @@
+@echo off
+set DOCUMENT_NAME=RAPPORT_A17_INFO_ST40_PINARD_MAXIME
+echo.
+for %%X in  (.acn .acr .alg .aux .bbl .blg .glg .glo .gls .ist .lof .log .out .toc .synctex.gz .pdf) do (
+	if exist %DOCUMENT_NAME%%%X (
+		echo Removing file %DOCUMENT_NAME%%%X
+		del %DOCUMENT_NAME%%%X >NUL 2>&1
+	)
+)
+echo.
