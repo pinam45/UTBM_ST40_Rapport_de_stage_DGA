735b731f25145242719fd3750675ed6783887714
Maxime Pinard <maxime.pin@live.fr>
2018-02-07 09:14:06 +0100

    PRESENTATION_DGA: GenDbg / Développement du module / Informations détaillées / Correspondance des registres

---
+++ code/tikz_MIPS_Capstone_FPU_SPR.c
@@ -0,0 +1,15 @@
+typedef enum mips_reg {
+	MIPS_REG_INVALID = 0,!\tikz[remember picture] \node (mips_reg_INVALID) {};!
+	MIPS_REG_PC,
+	...
+	MIPS_REG_FCC0,!\tikz[remember picture] \node (mips_reg_FCC0) {};!
+	MIPS_REG_FCC1,!\tikz[remember picture] \node (mips_reg_FCC1) {};!
+	MIPS_REG_FCC2,!\tikz[remember picture] \node (mips_reg_FCC2) {};!
+	MIPS_REG_FCC3,!\tikz[remember picture] \node (mips_reg_FCC3) {};!
+	MIPS_REG_FCC4,!\tikz[remember picture] \node (mips_reg_FCC4) {};!
+	MIPS_REG_FCC5,!\tikz[remember picture] \node (mips_reg_FCC5) {};!
+	MIPS_REG_FCC6,!\tikz[remember picture] \node (mips_reg_FCC6) {};!
+	MIPS_REG_FCC7,!\tikz[remember picture] \node (mips_reg_FCC7) {};!
+	...
+	MIPS_REG_ENDING
+} mips_reg;
---
+++ code/tikz_MIPS_GenDbg_FPU_SPR.c
@@ -0,0 +1,11 @@
+typedef enum {
+	 MIPS32_GPR_0 = 0, 
+	 ...
+	!\tikz[remember picture] \node (MIPS_RegisterId_T_FIR) {};!MIPS_FIR,
+	!\tikz[remember picture] \node (MIPS_RegisterId_T_FCCR) {};!MIPS_FCCR,
+	!\tikz[remember picture] \node (MIPS_RegisterId_T_FEXR) {};!MIPS_FEXR,
+	!\tikz[remember picture] \node (MIPS_RegisterId_T_FENR) {};!MIPS_FENR,
+	!\tikz[remember picture] \node (MIPS_RegisterId_T_FCSR) {};!MIPS_FCSR,
+	 ...
+	 MIPS_RegisterIdLast
+} MIPS_RegisterId_T;
